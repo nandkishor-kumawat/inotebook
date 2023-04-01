bbda70abf737ad112cb3930bc3a8f5a46c8daa4f
rohsikdnan <90635399+rohsikdnan@users.noreply.github.com>
2023-04-02 02:56:03 +0530

    Create auth.js

---
+++ backend/routes/auth.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const User = require('../models/User');
+const router = express.Router();
+const { body, validationResult } = require('express-validator');
+
+//POST: /api/auth/createuser
+router.post('/createuser', [
+    body('name', 'Enter a valid name').isLength({ min: 3 }),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password must be atleat 5 char').isLength({ min: 5 }),
+], async (req, res) => {
+
+    //handle errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    //check email exits or not 
+    try {
+        let user = await User.findOne({ email: req.body.email });
+        if (user) {
+            return res.status(400).json({ error: "Email already exists" });
+        }
+
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        })
+        res.json(user);
+
+    } catch (error) {
+        console.log({ error: error.message });
+        res.status(500).send("Some error occured")
+    }
+})
+
+module.exports = router
