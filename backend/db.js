775029dba2137b98eccf721b513295d859784b6f
rohsikdnan <90635399+rohsikdnan@users.noreply.github.com>
2023-04-03 16:50:59 +0530

    d

--- backend/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const mongoURI = 'mongodb://localhost:27017/inotebook'
-
-const connetToMongo = ()=>{
-    mongoose.connect(mongoURI)
-}
-
-module.exports = connetToMongo
